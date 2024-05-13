d444fc11cc22eb3634448c235f860fd3b7f5f410
Anglabr <angelina.labroska@gmail.com>
2024-05-13 18:06:23 +0200

    Initial commit

---
+++ clubs-management/src/main/java/mk/ukim/finki/emt/clubsmanagement/ClubsManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.clubsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubsManagementApplication.class, args);
+	}
+
+}
